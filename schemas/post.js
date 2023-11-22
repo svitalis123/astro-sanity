9ac1cb565299a9f12d50c0cf0c5ed27ad9e8d8bd
svitalis123 <vitalis.wilson@yahoo.com>
2023-11-22 21:29:53 +0300

    sanity blog

--- schemas/post.js
+++ schemas/post.js
@@ -20,26 +20,6 @@ export default defineType({
       },
     }),
     defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    }),
-    defineField({
       name: 'publishedAt',
       title: 'Published at',
       type: 'datetime',
